07d71f06a6431e1cd462b0689a7fd63feefdfe44
Deepak Choudhary <choudharydeepak938@gmail.com>
2020-12-12 00:22:29 +0530

    trail 1

--- src/app.js
+++ src/app.js
@@ -11,8 +11,33 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// your code goes here
+const mario = new marioModel({
+    name:"Luigi",
+    weight: 60
+})
 
+// mario.save();
+
+app.get("/mario", async(req,res)=>{
+
+    let characters = await marioModel.find({});
+    res.send(characters);
+});
+
+app.get("/mario/:id", async(req,res)=>{
+
+    let id = req.params.id;
+
+    try {
+        
+        let characters = await marioModel.find({_id:id});
+        res.send(characters[0]);
+        return;
+
+    } catch (error) {
+        res.status(400).send({message: error.message});  
+    }
+});
 
 
 module.exports = app;
